6cd597e2a548df1cd96a49c54a796ce586f0e201
neelkshah <46041693+neelkshah@users.noreply.github.com>
2018-12-28 14:07:40 +0530

    Update HazardDetectionUnit.v

--- HazardDetectionUnit.v
+++ HazardDetectionUnit.v
@@ -3,7 +3,7 @@ module HazardDetectionUnit(stall, Rs1, Rt1, Rt2, MemRead2, clk);
 	input [4:0] Rs1, Rt1, Rt2;
 	output [2:0] stall;
 	
-	always (@ posedge clk) begin
+	always (@ negedge clk) begin
 		if(MemRead2 & ((Rt2 = Rs1) | (Rt2 = Rt1)))
 			stall <= 3'b000;
 		else
